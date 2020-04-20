1a2174bc6973e99fd25d4d40f2023d471506f1ea
danir <danirg10000@gmail.com>
2020-04-20 14:51:40 +0300

    * added post call * commented out the date field

--- src/TodoAPI.kt
+++ src/TodoAPI.kt
@@ -1,28 +1,75 @@
 package com.ktor.sample
 
 import io.ktor.application.call
-import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
+import io.ktor.request.receive
 import io.ktor.response.respond
-import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
-import io.ktor.routing.method
+import io.ktor.routing.post
 import io.ktor.routing.route
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.lang.Exception
 
 fun Routing.todoApi() {
+    val logger: Logger = LoggerFactory.getLogger(Routing::class.java)
     route("api") {
-        route("todo", HttpMethod.Get) {
-            handle {
-                call.respond(todoList)
-            }
+        get("todo") {
+            logger.debug("get todo list")
+            call.respond(todoList)
         }
 
-        route("todo", HttpMethod.Post) {
-            handle {
-                call.respond(HttpStatusCode.Unauthorized)
+        get("todo/{id}") {
+            val id: String = call.parameters["id"]!!
+
+            try {
+                val todoItem = todoList[id.toInt()]
+                call.respond(todoItem)
+            } catch (e: Throwable) {
+                call.respond(HttpStatusCode.NotFound)
             }
+
         }
+
+        post("todo") {
+            logger.debug("hi")
+            try {
+                val todoItem: TodoItem = call.receive()
+                logger.debug(todoItem.toString())
+                val result = todoItem as? TodoItem ?: logger.debug("error converting to list item")
+                logger.debug(result.toString())
+                val newItem = todoItem.copy(id = 45)
+                todoList = todoList + todoItem + newItem
+                call.respond(HttpStatusCode.Created, todoItem)
+            } catch (e: Exception) {
+                logger.debug("failed to parse the item data")
+            }
+            logger.debug("after respond")
+
+
+        }
+
+        /*    route("todo", HttpMethod.Get) {
+                handle {
+                    call.respond(todoList)
+                }
+            }
+            curl -i -X GET -H "Accept: application/json" http://localhost:8081/api/todo/0
+            curl -i -X GET -H "Accept: application/json" -d @postTodo.json http://localhost:8081/api/todo
+            curl -i -X GET -H "Accept: application/json" -d '{
+"title":"a",
+"details":"b",
+"assignedTo":"c",
+"dueDate":"2020-3-20",
+"importance":"HIGH"
+}' http://localhost:8081/api/todo
+
+
+ curl -i -X GET -H "Accept: application/json" -d '{\"id\":\"5\",\"title\":\"a\",\"details\":\"b\",\"assignedTo\":\"c\",\"dueDate\":\"2020-3-15\",\"importance\":\"HIGH\"}' http://localhost:8081/api/todo
+
+
+            */
 
     }
 }
--- src/TodoItem.kt
+++ src/TodoItem.kt
@@ -7,33 +7,37 @@ import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer
 import java.time.LocalDate
 
 data class TodoItem(
-    val titles: String,
+    val id: Int,
+    val title: String,
     val details: String,
-    val assignedTo: String,
+    val assignedTo: String, /*
     @JsonSerialize(using = ToStringSerializer::class)
     @JsonDeserialize(using = LocalDateDeserializer::class)
-    val dueDate: LocalDate,
+    val dueDate: LocalDate, */
     val importance: Importance
 )
+
 enum class Importance {
     LOW, MEDIUM, HIGH
 }
 
 val todo1 = TodoItem(
+    0,
     "app REST API data access",
     "details",
     "me",
-    LocalDate.of(2020, 4, 12),
+   // LocalDate.of(2020, 4, 12),
     Importance.HIGH
 )
 
 
 val todo2 = TodoItem(
+    1,
     "Add REST API service",
     "more details",
     "me also",
-    LocalDate.of(2020, 4, 13),
+  //  LocalDate.of(2020, 4, 13),
     Importance.LOW
 )
 
-val todoList = listOf(todo1, todo2)
+var todoList = listOf(todo1, todo2)
